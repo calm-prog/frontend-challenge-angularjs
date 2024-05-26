8b40892d6d1cc4f65d9433185598077063113b37
Ferenc Vass <vass.ferenc91@gmail.com>
2024-05-26 06:53:37 +0200

    feat: display images on the UI

--- app/app.js
+++ app/app.js
@@ -1,13 +1,60 @@
-'use strict';
+"use strict";
 
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+angular
+  .module("myApp", [
+    "ngRoute",
+    "myApp.view1",
+    "myApp.view2",
+    "myApp.configService",
+    "myApp.unsplashService",
+  ])
+  .config([
+    "$locationProvider",
+    "$routeProvider",
+    function ($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix("!");
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+      $routeProvider.otherwise({ redirectTo: "/view1" });
+    },
+  ])
+  .controller("ImageSearchController", [
+    "$scope",
+    "UnsplashService",
+    function ($scope, UnsplashService) {
+      $scope.query = "";
+      $scope.images = [];
+      $scope.page = 1;
+      $scope.perPage = 10;
+
+      $scope.searchImages = function () {
+        if ($scope.query.trim() === "") {
+          return;
+        }
+
+        $scope.page = 1;
+        $scope.images = [];
+
+        UnsplashService.searchImages($scope.query, $scope.page, $scope.perPage)
+          .then(function (data) {
+            $scope.images = data.results;
+          })
+          .catch(function (error) {
+            console.error("Error during image search:", error);
+          });
+      };
+
+      $scope.loadMore = function () {
+        $scope.page++;
+
+        UnsplashService.searchImages($scope.query, $scope.page, $scope.perPage)
+          .then(function (data) {
+            $scope.images = $scope.images.concat(data.results);
+          })
+          .catch(function (error) {
+            console.error("Error loading more images:", error);
+          });
+      };
+
+      $scope.searchImages();
+    },
+  ]);
