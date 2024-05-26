5422095c43f7eb49436df1ca546985ab82ab62f5
Ferenc Vass <vass.ferenc91@gmail.com>
2024-05-26 14:55:51 +0200

    feat: style and animate images

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 angular
   .module("myApp", [
     "ngRoute",
+    "ngAnimate",
     "myApp.landingView",
     "myApp.imagesView",
     "myApp.noResultsView",
@@ -40,6 +41,16 @@ angular
           .catch(function (error) {
             console.error("Error during image search:", error);
           });
+      };
+
+      $scope.getImageOrientation = function (image) {
+        const { width, height } = image;
+
+        if (width >= height) {
+          return "landscape";
+        } else if (height > width) {
+          return "portrait";
+        }
       };
 
       $scope.loadMore = function () {
